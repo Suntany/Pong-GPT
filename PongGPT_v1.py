6eca6a629c5e87a023fb606df4f16d713e25243b
Songmel <shj3269@gmail.com>
2023-07-17 01:00:19 +0900

    tracking orange ball

--- PongGPT_v1.py
+++ PongGPT_v1.py
@@ -1,23 +1,41 @@
 # import the necessary packages
+
+# 콜렉션 모듈에서 deque라는 데이터 처리 함수
 from collections import deque
+#
 from imutils.video import VideoStream
+# numpy 모듈 임포트
 import numpy as np
+# argparse 모듈 임포트 (파이썬 스크립트를 명령 프롬프트에서 실행할 때 명령행 인자를 파싱할 수 있게 한다)
 import argparse
+# OpenCV 모듈 임포트
 import cv2
+# imutils 모듈 임포트 (이미지 프로세싱 관련된 유용한 기능들 있다)
 import imutils
+# time 모듈 임포트
 import time
+
 # construct the argument parse and parse the arguments
+
+# parser를 만든다 이름은 ap
 ap = argparse.ArgumentParser()
+
+# add_argument 메서드로 받아들일 인수를 추가해나간다
+# -v 로 비디오 파일을 선택해서 재생할 수 있다.
 ap.add_argument("-v", "--video",
 	help="path to the (optional) video file")
+
+# -b 로 최대 버퍼 사이즈를 지정할 수 있다 기본은 64
 ap.add_argument("-b", "--buffer", type=int, default=64,
 	help="max buffer size")
+
 args = vars(ap.parse_args())
-# define the lower and upper boundaries of the "green"
+
+# define the lower and upper boundaries of the "Orange"
 # ball in the HSV color space, then initialize the
 # list of tracked points
-greenLower = (29, 86, 6)
-greenUpper = (64, 255, 255)
+orangeLower = (17, 255, 150)
+orangeUpper = (34, 255, 255)
 pts = deque(maxlen=args["buffer"])
 # if a video path was not supplied, grab the reference
 # to the webcam
@@ -44,10 +62,10 @@ while True:
 	frame = imutils.resize(frame, width=600)
 	blurred = cv2.GaussianBlur(frame, (11, 11), 0)
 	hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
-	# construct a mask for the color "green", then perform
+	# construct a mask for the color "orange", then perform
 	# a series of dilations and erosions to remove any small
 	# blobs left in the mask
-	mask = cv2.inRange(hsv, greenLower, greenUpper)
+	mask = cv2.inRange(hsv, orangeLower, orangeUpper)
 	mask = cv2.erode(mask, None, iterations=2)
 	mask = cv2.dilate(mask, None, iterations=2)
 	# find contours in the mask and initialize the current
