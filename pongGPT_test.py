d14e7f3eb9f4f5069a7f8750f342783f7ad4a3fd
Songmel <35496730+Songmel@users.noreply.github.com>
2023-07-29 22:52:20 +0900

    pongGPT_test make

--- pongGPT_test.py
+++ pongGPT_test.py
@@ -30,8 +30,8 @@ RALLY_COUNT = 0
 FINAL_XY = None
 
 # 주황색 탁구공 HSV 색 범위 지정
-orangeLower = (4, 158, 240)
-orangeUpper = (25, 255, 255)
+orangeLower = (1, 130, 240)
+orangeUpper = (30, 255, 255)
 
 # 파서 코딩 부분
 ap = argparse.ArgumentParser()
@@ -68,66 +68,12 @@ while True:
         ((x, y), radius) = cv2.minEnclosingCircle(c)
         M = cv2.moments(c)
         center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+        
+        print(center)
         # 최소 지름 넘겼을 경우 원 그리기
         if radius > 5:
             cv2.circle(frame, (int(x), int(y)), int(radius), (0, 255, 255), 2)
             cv2.circle(frame, center, 5, (0, 0, 255), -1)
-
-            # 탁구 알고리즘 실행
-            # line1
-            if ball_in_1 == False and center[1] > 0 and center[1] < CENTER_LINE:
-                if LINE1_TOGGLE == True:
-                    LINE1_TOGGLE = False
-                else:
-                    LINE1_TOGGLE = True
-                ball_in_1 = True
-                ball_in_2 = False
-
-                LINE1_XY = center
-                print("line1 detect : ", end="")
-                print(center)
-
-            # line2
-            if ball_in_2 == False and center[1] > CENTER_LINE and center[1] < 853:
-                if LINE2_TOGGLE == True:
-                    LINE2_TOGGLE = False
-                else:
-                    LINE2_TOGGLE = True
-                ball_in_1 = False
-                ball_in_2 = True
-
-                LINE2_XY = center
-                print("line2 detect : ", end="")
-                print(center)
-
-            # line calculating
-            if LINE1_TOGGLE == True and LINE2_TOGGLE == True and line_ison == False:
-                FINAL_XY = (
-                    0,
-                    int(
-                        LINE1_XY[1]
-                        - (LINE1_XY[0])
-                        * (LINE1_XY[1] - LINE2_XY[1])
-                        / (LINE1_XY[0] - LINE2_XY[0])
-                    ),
-                )
-                line_ison = True
-                print("FINAL_XY : ", end="")
-                print(center)
-
-            # reset for next & rally pointing
-            if LINE1_TOGGLE == False and LINE2_TOGGLE == False and line_ison == True:
-                RALLY_COUNT += 1
-                LINE1_XY = None
-                LINE2_XY = None
-                FINAL_XY = None
-                line_ison = False
-                print("RALLY_COUNT : ", end="")
-                print(RALLY_COUNT)
-
-    else:
-        ball_in_1 = False
-        ball_in_2 = False
 
     # 트레킹 레드라인 코드
     pts.appendleft(center)
@@ -137,19 +83,6 @@ while True:
         thickness = int(np.sqrt(args["buffer"] / float(i + 1)) * 2.5)
         cv2.line(frame, pts[i - 1], pts[i], (0, 0, 255), thickness)
 
-    # 화면 표시 선 코드
-    if LINE1_TOGGLE == True:
-        cv2.rectangle(frame, LINE1_BOX, GREEN_BGR, 5)
-    else:
-        cv2.rectangle(frame, LINE1_BOX, RED_BGR, 5)
-
-    if LINE2_TOGGLE == True:
-        cv2.rectangle(frame, LINE2_BOX, GREEN_BGR, 5)
-    else:
-        cv2.rectangle(frame, LINE2_BOX, RED_BGR, 5)
-
-    if line_ison == True:
-        cv2.line(frame, FINAL_XY, LINE2_XY, (255, 0, 0), 5)
 
     cv2.line(frame, (500,0), (500,600), (0,200,0), 1)
 
