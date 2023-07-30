19eecdc1862ab22eb918561c46268b6e287d54f2
Songmel <35496730+Songmel@users.noreply.github.com>
2023-07-30 20:00:31 +0900

    hsv change v3

--- pongGPT_v3.py
+++ pongGPT_v3.py
@@ -16,19 +16,19 @@ CENTER_LINE = 500  # 세로 센터 라인
 NET_LINE = 640  # 네트 라인
 
 CATCH_FRAME = 4
-MIN_GAP = 50
+MIN_GAP = 10
 
 # 초기화 변수들
 ball_in = False
 line_on = False
 
 RALLY_COUNT = 0
-FINAL_MOVE = None  # 단위 cm
-FINAL_ETA = None  # 단위 ms
+FINAL_MOVE = 0  # 단위 cm
+FINAL_ETA = 0  # 단위 ms
 
 # 주황색 탁구공 HSV 색 범위 지정 (창문쪽 형광등 두 개 키고 문쪽 형광등 한 개 껐을때 기준)
-orangeLower = (4, 158, 240)
-orangeUpper = (25, 255, 255)
+orangeLower = (1, 130, 240)
+orangeUpper = (30, 255, 255)
 
 # 파서 코딩 부분
 ap = argparse.ArgumentParser()
@@ -46,7 +46,6 @@ temp_speed = deque()  # 단위 px/ms
 
 # Line Activater 쓰레드 함수
 def line_activator(ETA):
-    line_on = True
     print("Line Activated / Detecting LOCK")
     time.sleep(ETA)
     line_on = False
@@ -54,15 +53,12 @@ def line_activator(ETA):
     line_xy.clear()
     temp_move.clear()
     temp_speed.clear()
-    line_on = False
     FINAL_MOVE = None
     FINAL_ETA = None
 
 
 # 쓰레드 생성
-newline_act = threading.Thead(
-    target=line_activator, args=(FINAL_ETA / 1000,), daemon=True
-)
+
 
 # 비디오 스트리밍 시작
 vs = VideoStream(src=VIDEO_SELECTION).start()
@@ -95,7 +91,7 @@ while True:
 
         # 탁구 알고리즘
         if line_on == False:
-            print(center)
+            #print(center)
             line_xy.append(center)
             time_xy.append(time.time())
             if len(line_xy) == 2:
@@ -110,7 +106,7 @@ while True:
                     )
                     temp_speed.append(
                         int(
-                            sqrt(
+                            np.sqrt(
                                 (line_xy[0][0] - line_xy[1][0]) ** 2
                                 + (line_xy[0][1] - line_xy[1][1]) ** 2
                             )
@@ -119,19 +115,23 @@ while True:
                     )
 
         if len(temp_move) == CATCH_FRAME:
+            #디버깅
+            print(temp_speed)
+            print(line_xy[1])
+
             temp_move.popleft()
             temp_speed.popleft()
 
             temp_move_sum = 0
             for i in range(CATCH_FRAME - 1):
-                temp_sum += temp_move.popleft()
-            FINAL_MOVE = int(temp_sum / (CATCH_FRAME - 1) * (152.5 / 680))
+                temp_move_sum += temp_move.popleft()
+            FINAL_MOVE = int(temp_move_sum / (CATCH_FRAME - 1) * (152.5 / 680))
 
             temp_speed_sum = 0
             for i in range(CATCH_FRAME - 1):
                 temp_speed_sum += temp_speed.popleft()
             FINAL_ETA = int(
-                sqrt(
+                np.sqrt(
                     (line_xy[1][0] - FINAL_MOVE * (680 / 152.5)) ** 2
                     + (line_xy[1][1] - 1220) ** 2
                 )
@@ -141,7 +141,11 @@ while True:
             print(
                 "FINAL MOVE : {0}cm / FINAL ETA : {1}ms".format(FINAL_MOVE, FINAL_ETA)
             )
+            line_on = True
+        
+            newline_act = threading.Thread(target=line_activator, args=(FINAL_ETA / 1000,), daemon=True)
             newline_act.start()
+
 
     # 트레킹 레드라인 코드
     pts.appendleft(center)
